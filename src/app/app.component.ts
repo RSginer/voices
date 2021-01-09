3cc54e5b25a100ca3a7618291227d887c75daeaa
Rubén Soler <r.solerginer@gmail.com>
2021-01-09 21:55:17 +0100

    Feature titled section and voice components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Voice } from './core/models/Voice';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'voices';
+  favoriteVoices: Voice[] = [];
+  voices: Voice[] = [];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VoiceComponent } from './components/voice/voice.component';
+import { TitledSectionComponent } from './components/titled-section/titled-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoiceComponent,
+    TitledSectionComponent
   ],
   imports: [
     BrowserModule
