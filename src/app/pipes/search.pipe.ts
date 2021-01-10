8c3c6a5ab57c25f016b9b409a977f368eb687016
Rubén Soler <r.solerginer@gmail.com>
2021-01-10 01:43:48 +0100

    Lint

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -2,21 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Voice } from '../core/models/Voice';
 
 @Pipe({
-    name: 'search'
+  name: 'search'
 })
 
 export class SearchPipe implements PipeTransform {
-    transform(items: Voice[], searchText: string | undefined): any {
-        if (!items) {
-            return [];
-          }
-          if (!searchText) {
-            return items;
-          }
-          searchText = searchText.toLocaleLowerCase();
-      
-          return items.filter(it => {
-            return it.name.toLocaleLowerCase().includes(searchText || "");
-          });
+  transform(items: Voice[], searchText: string | undefined): any {
+    if (!items) {
+      return [];
     }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.name.toLocaleLowerCase().includes(searchText || "");
+    });
+  }
 }
